6b94464da817e8901cefbe8ca9022d7ee0e27ebf
lipex360x <lipex360@gmail.com>
2022-06-28 01:39:32 -0300

    Game Finished

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,20 +1,17 @@
-import { useState } from 'react';
 import * as S from './styles';
 
 export type CardProps = {
   id: string;
-  back: string;
+  back?: string;
+  flipped?: boolean;
+  handleClick: (id: string) => void;
 };
 
-export function Card({ id, back }: CardProps) {
-  const [flipped, setFlipped] = useState(false);
-
-  function handleClick() {
-    setFlipped(!flipped);
-  }
+export function Card({ id, flipped = false, handleClick, back }: CardProps) {
+  const handleClickFn = () => handleClick(id);
 
   return (
-    <S.Wrapper flipped={flipped} onClick={handleClick}>
+    <S.Wrapper flipped={flipped} onClick={handleClickFn}>
       <div className="face front">?</div>
       <div className="face back">{back}</div>
     </S.Wrapper>
--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,16 +1,11 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { duplicateRegenerateSortArray } from '../../utils/arrayFunctions';
 
-import { Card } from '../Card';
+import { Card, CardProps } from '../Card';
 import * as S from './styles';
 
 export type GridProps = {
   cards: CardProps[];
-};
-
-type CardProps = {
-  id: string;
-  flipped: boolean;
 };
 
 export function Grid({ cards }: GridProps) {
@@ -18,11 +13,79 @@ export function Grid({ cards }: GridProps) {
     return duplicateRegenerateSortArray(cards);
   });
 
+  const first = useRef<CardProps | null>(null);
+  const second = useRef<CardProps | null>(null);
+  const unflip = useRef(false);
+
+  const [matches, setMatches] = useState(0);
+  const [moves, setMoves] = useState(0);
+
+  const handleReset = () => {
+    setStatesCards(duplicateRegenerateSortArray(cards));
+    first.current = null;
+    second.current = null;
+    unflip.current = false;
+
+    setMatches(0);
+    setMoves(0);
+  };
+
+  const handleClick = (id: string) => {
+    const newStateCards = stateCards.map((card) => {
+      if (card.id !== id) return card;
+
+      if (card.flipped) return card;
+
+      if (unflip.current && first.current && second.current) {
+        first.current.flipped = false;
+        second.current.flipped = false;
+
+        first.current = null;
+        second.current = null;
+        unflip.current = false;
+      }
+
+      card.flipped = true;
+
+      if (first.current === null) {
+        first.current = card;
+      } else if (second.current === null) {
+        second.current = card;
+      }
+
+      if (first.current && second.current) {
+        if (first.current.back === second.current.back) {
+          first.current = null;
+          second.current = null;
+
+          setMatches((match) => match + 1);
+        } else {
+          unflip.current = true;
+        }
+
+        setMoves((move) => move + 1);
+      }
+
+      return card;
+    });
+
+    setStatesCards(newStateCards);
+  };
+
   return (
-    <S.Wrapper>
-      {stateCards.map((card, index) => (
-        <Card key={index} {...card} />
-      ))}
-    </S.Wrapper>
+    <>
+      <div className="text">
+        <h1>Memory Game</h1>
+        <p>
+          Moves: {moves} | matches {matches} |{' '}
+          <button onClick={handleReset}>Reset</button>
+        </p>
+      </div>
+      <S.Wrapper>
+        {stateCards.map((card, index) => (
+          <Card key={index} {...card} handleClick={handleClick} />
+        ))}
+      </S.Wrapper>
+    </>
   );
 }
